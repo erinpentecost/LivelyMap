9757351cbac38305474641cec954cbd641ad7dfa
Erin Pentecost <erin@pentecost.email>
2025-12-06 22:05:46 -0800

    docs

---
+++ sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pushd "$(dirname "$0")" || exit
+pwd
+
+# build the tool if it's not already built
+if [ ! -f "./cmd/lively" ]; then
+pushd ./cmd/lively
+go build . || exit
+popd
+fi
+
+# run it
+shift
+./cmd/lively/lively $@
+
+popd
