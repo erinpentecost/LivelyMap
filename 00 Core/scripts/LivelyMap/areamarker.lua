24582a0a519228e768dd3f62799aacf176d24ef4
Erin Pentecost <erin@pentecost.email>
2026-01-01 22:24:48 -0800

    better journal marking

--- 00 Core/scripts/LivelyMap/areamarker.lua
+++ 00 Core/scripts/LivelyMap/areamarker.lua
@@ -39,6 +39,7 @@ local cellData     = storage.globalSection(MOD_NAME .. "_cellNames"):asTable()
 
 local settingCache = {
     autoMarkNamedExteriorCells = settings.automatic.autoMarkNamedExteriorCells,
+    autoMarkFromJournal = settings.automatic.autoMarkFromJournal
 }
 
 settings.automatic.subscribe(async:callback(function(_, key)
@@ -112,14 +113,7 @@ local function onReceiveExteriorLocation(data)
 end
 
 
-local function onQuestUpdate(questId, stage)
-    print("quest update!!")
-    print(aux_util.deepToString(types.Player.journal(pself).topics, 4))
-    -- test with:
-    -- Journal, "FG_Egg_Poachers", 1
-    -- Journal, "FG_VerethiGang", 10
-    --
-
+local function markFromJournal()
     -- build map of canonicalized topics
     local topics = {}
     for _, topic in pairs(types.Player.journal(pself).topics) do
@@ -135,6 +129,12 @@ local function onQuestUpdate(questId, stage)
         end
     end
 end
+
+interfaces.LivelyMapDraw.onMapMoved(function(data)
+    if (not data.swapped) and (settingCache.autoMarkFromJournal) then
+        markFromJournal()
+    end
+end)
 
 local lastExteriorCell = nil
 local delta = 0
@@ -168,6 +168,5 @@ return {
     },
     engineHandlers = {
         onUpdate = onUpdate,
-        onQuestUpdate = onQuestUpdate
     }
 }
