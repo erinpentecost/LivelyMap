49340d50ee951114dfc91ba7080834a010606880
Erin Pentecost <erin@pentecost.email>
2026-01-17 13:52:46 -0800

    wip

--- internal/hdmap/color.go
+++ internal/hdmap/color.go
@@ -79,11 +79,14 @@ func (d *ColorRenderer) Render(p *ParsedLandRecord) *image.RGBA {
 			iy := gridSize - y - 1
 			baseColor := d.ramp.Color(p.heights[y][x], d.minHeight, d.maxHeight, d.waterHeight)
 			if p.heights[y][x] >= d.waterHeight {
-				//ty := iy / 4
-				ty := (gridSize - 1 - y) / 4 // flip before 4x4 reduction
-				tx := x / 4
-				if len(p.vtex) == 16 && len(p.vtex[ty]) == 16 {
-					texIndex := p.vtex[ty][tx]
+				gx := x / 4
+				gy := y / 4
+
+				dx := 4*(gy%4) + (gx % 4)
+				dy := 4*(gy/4) + (gx / 4)
+
+				if len(p.vtex) == 16 && len(p.vtex[dy]) == 16 {
+					texIndex := p.vtex[dy][dx]
 					tex, ok := d.textures[texIndex]
 					if ok {
 						baseHSL := hue.RGBToHSL(baseColor)
@@ -92,17 +95,14 @@ func (d *ColorRenderer) Render(p *ParsedLandRecord) *image.RGBA {
 					}
 				}
 
-				// TODO: un-disable?
-				if false {
-					// set the hue from the vertex color
-					if len(p.colors) == 65 && len(p.colors[y]) == 65 {
-						baseColor = hue.MulColor(baseColor, color.RGBA{
-							R: p.colors[y][x].R,
-							G: p.colors[y][x].G,
-							B: p.colors[y][x].B,
-							A: math.MaxUint8,
-						})
-					}
+				// multiply by vertex color
+				if len(p.colors) == 65 && len(p.colors[y]) == 65 {
+					baseColor = hue.MulColor(baseColor, color.RGBA{
+						R: p.colors[y][x].R,
+						G: p.colors[y][x].G,
+						B: p.colors[y][x].B,
+						A: math.MaxUint8,
+					})
 				}
 			}
 
@@ -110,83 +110,4 @@ func (d *ColorRenderer) Render(p *ParsedLandRecord) *image.RGBA {
 		}
 	}
 	return img
-}
-
-func (d *ColorRenderer) renderHueFromTex(p *ParsedLandRecord) *image.RGBA {
-	img := image.NewRGBA(image.Rect(0, 0, gridSize, gridSize))
-
-	// Throw away the last column and row.
-	// This is how I'm sampling a quad into a single pixel.
-	for y := range gridSize {
-		for x := range gridSize {
-			// Need to invert y
-			iy := gridSize - y - 1
-			baseColor := d.ramp.Color(p.heights[y][x], d.minHeight, d.maxHeight, d.waterHeight)
-			if p.heights[y][x] >= d.waterHeight {
-				// set the hue from the texture
-				ty := iy / 4
-				tx := x / 4
-				// get color from texture map if available
-				if len(p.vtex) == 16 && len(p.vtex[ty]) == 16 {
-					texIndex := p.vtex[ty][tx]
-					tex, ok := d.textures[texIndex]
-					if ok {
-						baseHSL := hue.RGBToHSL(baseColor)
-						baseHSL.H = tex.avgHue
-						baseColor = hue.HSLToRGB(baseHSL)
-					}
-				}
-			}
-
-			img.SetRGBA(x, iy, baseColor)
-		}
-	}
-	return img
-}
-
-func (d *ColorRenderer) transformHeight(v float32) byte {
-	// clamp extremes
-	if v <= d.minHeight {
-		return 0
-	}
-	if v >= d.maxHeight {
-		return 0xFF // 255
-	}
-
-	// exact water line -> the first value of the upper half
-	if v == d.waterHeight {
-		return 128
-	}
-
-	if v < d.waterHeight {
-		denom := d.waterHeight - d.minHeight
-		if denom == 0 {
-			return 0
-		}
-		normalized := float64((v - d.minHeight) / denom) // 0..1
-		// map to 0..127 (128 values)
-		val := math.Round(normalized * 127.0)
-		if val < 0 {
-			val = 0
-		}
-		if val > 127 {
-			val = 127
-		}
-		return byte(uint8(val))
-	}
-
-	// v > waterHeight -> map to 128..255 (128 values)
-	denom := d.maxHeight - d.waterHeight
-	if denom == 0 {
-		return 0xFF
-	}
-	normalized := float64((v - d.waterHeight) / denom) // 0..1
-	val := math.Round(normalized * 127.0)              // 0..127
-	if val < 0 {
-		val = 0
-	}
-	if val > 127 {
-		val = 127
-	}
-	return byte(uint8(128 + int(val)))
 }
